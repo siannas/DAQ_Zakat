06955cfd9f3f4ab6778f04e77e1baa0c47afecfc
siannas <massive.annas@gmail.com>
2019-03-07 14:12:18 +0700

    added login and register

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,3 @@ Route::get('/dashboard', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-// Route::get('/dashboard', 'UserController@todashboard')->name('dashboard');
